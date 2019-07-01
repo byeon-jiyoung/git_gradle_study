3b7887ba8c7c6832632c3ef29801b75adb791f41
변지영 <bjy8668@hanmail.net>
2019-07-01 14:41:23 +0900

    한줄 추가함.

--- src/main/java/git_gradle_study/HelloWorld.java
+++ src/main/java/git_gradle_study/HelloWorld.java
@@ -3,5 +3,6 @@ package git_gradle_study;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("World's end Deancehall");
 	}
 }
